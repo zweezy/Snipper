594e00c9c9b01b5cde089aaa9c87307aa2ebc98a
Zwick Shaw <zashaw@ncsu.edu>
2016-04-22 20:22:22 -0400

    A little tool I just made for snipping pics from the screen.

---
+++ install_snipper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo mkdir /usr/share/icons/zweezy_snipper
+sudo mv ./screenshot.ico /usr/share/icons/zweezy_snipper/screenshot.ico
+sudo mv ./screen_shot.desktop /usr/share/applications/screen_shot.desktop
