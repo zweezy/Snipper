bac31c8ee0eface693bb927c17d1b4080546308b
Zwick Shaw <zweezy@users.noreply.github.com>
2016-06-07 23:53:23 -0400

    Update install_snipper.sh

--- install_snipper.sh
+++ install_snipper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo mkdir /usr/share/icons/zweezy_snipper
 sudo mv screen_shot.ico /usr/share/icons/zweezy_snipper/screen_shot.ico
-sudo mv screen_shot1.desktop /usr/share/applications/screen_shot.desktop
+sudo mv screen_shot.desktop /usr/share/applications/screen_shot.desktop
